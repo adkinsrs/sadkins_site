bd0bde7fa08d66e6289d184cf3119d05895d7298
adkinsrs <shaun.adkins@gmail.com>
2017-08-02 21:04:23 -0400

    prepping repo for jekyll website

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Remove embed when loading mobile site (width <= 720px)
-if (window.outerWidth <= 720){
-    var p_node = document.getElementById("text_container");
-    var c_node = document.getElementById("resume_file");
-    p_node.removeChild(c_node);
-}
